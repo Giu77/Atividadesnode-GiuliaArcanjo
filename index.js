57a456db549243e3911f158878bcae94d34b8a51
giulia <116807603+Giu77@users.noreply.github.com>
2024-08-27 23:06:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const nomeCompleto = "Giulia Arcanjo";
+const anoNascimento = 2007;
+let anoAtual = 2024;
+const idade = 17;
+
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano de " + anoAtual + " minha idade é " + idade + ".");
+
+let AnoAtual = 2025;
+let Idade = 18;
+const NomeCompleto = "Giulia Arcanjo ";
+const Faculdade = "farmacia";
+
+console.log("Olá meu nome é " + NomeCompleto + ", tenho " + Idade + " anos " +  "estamos no ano de " + AnoAtual + " gostaria de fazer faculdade na área de " + Faculdade);
+
+console.log(`oi eu sou ${nomeCompleto}`)
+
+const ListaDeViagens = new Array(
+  `França`,
+  `Londres`,
+  `Nova York`,
+  `Tokyio`,
+  `Barcelona`
+  );
+
+  console.log(ListaDeViagens);
+
+console.log(ListaDeViagens[1])
+ListaDeViagens.push("Coronel Vivida");
+console.log(ListaDeViagens)
+
+ListaDeViagens.splice(1,1);
+console.log(ListaDeViagens);
+
+const IdadeComprador = 18;
+
+if(IdadeComprador < 18){
+  console.log("Infelizmente, você é menor de idade e não podemos lhe vender a passagem")
+}
+
+if(IdadeComprador >= 18){
+  console.log("Parabéns, sua compra foi realizada com sucesso")
+}
